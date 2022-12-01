3a520a70374e929b16f93bd2bdd7da764c3c8f4b
divxouchi <manabu.ouchi@divx.co.jp>
2022-12-01 17:06:08 +0900

    ルーティング訂正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+
+    render json:@products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
  
-  resources :tweets, only: :index
+  resources :products, only: :index
 end
